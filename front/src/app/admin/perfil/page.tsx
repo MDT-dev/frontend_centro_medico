ced5c086252613d86640f774ac16a4550c1f83d5
Ananiassapalodeveloper <ananiassapalot@gmail.com>
2026-04-23 11:36:20 +0100

    jesus works

--- front/src/app/admin/perfil/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ProfileModule as Profile } from "@/components/profile/profile"
-
-export default function ProfilePage() {
-  return (
-    <div className="container mx-auto p-6">
-      <Profile />
-    </div>
-  )
-}
